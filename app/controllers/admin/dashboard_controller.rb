e6054e1ebcf8a74e0f9e228ac8585263c7f9d0b3
Tyler Shelton <tylershelton99@gmail.com>
2022-01-20 14:55:22 -0500

    Add variables to the admin controller that pulls the counts for the number of categories and number of products in the db. Then add those values to the admin dashboard page as well as a button to link you to the products admin page.

--- app/controllers/admin/dashboard_controller.rb
+++ app/controllers/admin/dashboard_controller.rb
@@ -1,4 +1,8 @@
 class Admin::DashboardController < ApplicationController
   def show
+    @total_categories = Category.count
+    puts @total_categories
+    @total_products = Product.count
+    puts @total_products
   end
 end
